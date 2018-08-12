e699428984657cc9bffafe72f767c7b7841adc17
sunlubo <sunlubo.sun@gmail.com>
2018-08-12 22:08:50 +0800

    update demo name 😅

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
         .executable(
             name: "SwiftSDL2Demo",
-            targets: ["Demo"]
+            targets: ["SwiftSDL2Demo"]
         )
     ],
     dependencies: [
@@ -23,8 +23,9 @@ let package = Package(
             name: "SwiftSDL2"
         ),
         .target(
-            name: "Demo",
-            dependencies: ["SwiftSDL2"]
+            name: "SwiftSDL2Demo",
+            dependencies: ["SwiftSDL2"],
+            path: "Sources/Demo"
         ),
         .testTarget(
             name: "SwiftSDL2Tests",
